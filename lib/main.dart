da95c38b97e084e875ffda89af547650d66997f6
Imam Firdaus Dwimeianto <idwimeianto@gmail.com>
2022-11-04 05:45:11 +0700

    add basic interface for login screen

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,133 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Directionality(
-        textDirection: TextDirection.ltr,
-        child: Text('Hello World!'),
+    return MaterialApp(
+      home: Scaffold(
+        resizeToAvoidBottomInset: true,
+        body: LayoutBuilder(builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                  minWidth: constraints.maxWidth,
+                  minHeight: constraints.maxHeight),
+              child: IntrinsicHeight(
+                child: Container(
+                  padding: const EdgeInsets.all(30),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(40),
+                        child: FittedBox(
+                          child: Image.asset('assets/hero.png'),
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                      Container(
+                        child: Column(
+                          children: [
+                            Text('Selamat Datang'),
+                            Text('di Aplikasi MAL SKA Membership'),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Column(
+                          children: [
+                            Text('Email'),
+                            LoginForm(),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Text(
+                            'Dengan Memakai aplikasi ini saya setudu dengan Persyaratan Layanan dan Kebijakan Pribadi'),
+                      ),
+                      Spacer(),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('SKA MALL Mobile 2021'),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
+
+class LoginForm extends StatefulWidget {
+  const LoginForm({super.key});
+
+  @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextFormField(
+            // The validator receives the text that the user has entered.
+            // validator: (value) {
+            //   if (value == null || value.isEmpty) {
+            //     return 'Please enter some text';
+            //   }
+            //   return null;
+            // },
+            decoration: InputDecoration(
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(
+                  width: 1,
+                  color: Color.fromARGB(255, 206, 212, 218),
+                ),
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Validate returns true if the form is valid, or false otherwise.
+                    // if (_formKey.currentState!.validate()) {
+                    //   // If the form is valid, display a snackbar. In the real world,
+                    //   // you'd often call a server or save the information in a database.
+                    //   ScaffoldMessenger.of(context).showSnackBar(
+                    //     const SnackBar(content: Text('Processing Data')),
+                    //   );
+                    // }
+                  },
+                  child: const Text('Masuk'),
+                ),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Daftar'),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
