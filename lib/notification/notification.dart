e7e12abad07293fa47b8db5682b30cb2b775a562
Imam Firdaus Dwimeianto <idwimeianto@gmail.com>
2022-11-04 07:29:37 +0700

    add notification basic interface

---
+++ lib/notification/notification.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:utsapp/shared/bottom_nav.dart';
+
+class NotificationScreen extends StatelessWidget {
+  const NotificationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Notifikasi'),
+        automaticallyImplyLeading: false,
+      ),
+      bottomNavigationBar: const BottomNavBar(),
+    );
+  }
+}
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,8 +1,10 @@
 import 'package:utsapp/home/home.dart';
 import 'package:utsapp/login/login.dart';
+import 'package:utsapp/notification/notification.dart';
 
 var appRoutes = {
   '/': (context) => const LoginScreen(),
   '/login': (context) => const LoginScreen(),
   '/home': (context) => const HomeScreen(),
+  '/notification': (context) => const NotificationScreen(),
 };
